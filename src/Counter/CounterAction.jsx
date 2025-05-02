87b4697fda0b1a792298c1b230c3a01295502e7c
Arbaj Ansari <arbaj897ansari@gmail.com>
2025-05-02 20:02:02 +0530

    made somce changes

--- src/Counter/CounterAction.jsx
+++ src/Counter/CounterAction.jsx
@@ -2,18 +2,18 @@ import React, { useContext } from "react";
 import { CounterContext } from "../context/CounterContext";
 
 const CounterAction = () => {
-  const { count, setCount } = useContext(CounterContext);
+  const { setCount } = useContext(CounterContext);
 
   return (
     <div className="flex justify-between gap-4 mt-6">
       <button
-        onClick={() => setCount(count - 1)}
+        onClick={() => setCount((prev) => prev - 1)}
         className="bg-black cursor-pointer text-white border border-gray-400 py-2 px-4 rounded-full hover:bg-gray-700 transition"
       >
         Decrement
       </button>
       <button
-        onClick={() => setCount(count + 1)}
+        onClick={() => setCount((prev) => prev + 1)}
         className="bg-white cursor-pointer text-black py-2 px-4 rounded-full hover:bg-gray-200 transition"
       >
         Increment
@@ -23,3 +23,11 @@ const CounterAction = () => {
 };
 
 export default CounterAction;
+
+// setCount function or value
+const value = null;
+setCount(value);
+
+const foo = function (previousValue) {
+  return previousValue + 9;
+};
